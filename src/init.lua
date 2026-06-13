c478bcf65e25367ea38dd96b029681ad52ab1604
noahchico52-png <noahchico52@gmail.com>
2026-06-13 08:03:43 -0400

    Rename folder and update GitHub paths

--- src/init.lua
+++ src/init.lua
@@ -3,9 +3,10 @@ if not game:IsLoaded() then
 end
 local env = getgenv()
 
-if not isfolder("BrainrotPolice") then makefolder("BrainrotPolice") end
-if not isfile("BrainrotPolice/Config.json") then
-    writefile("BrainrotPolice/Config.json", game:GetService("HttpService"):JSONEncode({
+-- Change folder name here if you want
+if not isfolder("MM2ScamPolice") then makefolder("MM2ScamPolice") end
+if not isfile("MM2ScamPolice/Config.json") then
+    writefile("MM2ScamPolice/Config.json", game:GetService("HttpService"):JSONEncode({
         settings = {
             auto_rejoin_on_kick = false,
             disable_3d_rendering = false
@@ -17,8 +18,9 @@ function env.import(id)
     return game:GetObjects(id)[1]
 end
 
+-- CHANGE THIS TO YOUR GITHUB
 function env.getgitpath(where)
-    local mainBuild = "https://raw.githubusercontent.com/IcantAffordSynapse/BrainrotPolice/refs/heads/main/"
+    local mainBuild = "https://raw.githubusercontent.com/noahchico52-png/grtoikgrejgre/refs/heads/main/"
     if where == "src" then
         return mainBuild .. "src/"
     elseif where == "games" then
@@ -28,10 +30,10 @@ end
 
 function env.setconfig(key, value)
     local httpservice = game:GetService("HttpService")
-    local dec = httpservice:JSONDecode(readfile("BrainrotPolice/Config.json"))
+    local dec = httpservice:JSONDecode(readfile("MM2ScamPolice/Config.json"))
     dec[tostring(game.PlaceId)] = dec[tostring(game.PlaceId)] or {}
     dec[tostring(game.PlaceId)][key] = value
-    writefile("BrainrotPolice/Config.json", httpservice:JSONEncode(dec))
+    writefile("MM2ScamPolice/Config.json", httpservice:JSONEncode(dec))
 end
 
 game:GetService("GuiService").ErrorMessageChanged:Connect(function()
@@ -40,8 +42,10 @@ game:GetService("GuiService").ErrorMessageChanged:Connect(function()
     end
 end)
 
+-- This loads YOUR ui.lua from YOUR GitHub
 loadstring(game:HttpGet(getgitpath("src").."ui.lua"))()
 
+-- CHANGE THIS TO YOUR GITHUB TOO
 if queue_on_teleport then
-    queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/IcantAffordSynapse/BrainrotPolice/refs/heads/main/src/init.lua"))()')
+    queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/noahchico52-png/grtoikgrejgre/refs/heads/main/src/init.lua"))()')
 end
